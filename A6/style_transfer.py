b685eeffcb858595bb129c8b8202de4b6ed0c707
pyskinas <vpysk@protonmail.com>
2024-02-15 15:22:52 +0000

    add A6

---
+++ A6/style_transfer.py
@@ -0,0 +1,214 @@
+"""
+Implements a style transfer in PyTorch.
+WARNING: you SHOULD NOT use ".to()" or ".cuda()" in each implementation block.
+"""
+
+import torch
+import torch.nn as nn
+from a6_helper import *
+
+def hello():
+  """
+  This is a sample function that we will try to import and run to ensure that
+  our environment is correctly set up on Google Colab.
+  """
+  print('Hello from style_transfer.py!')
+
+def content_loss(content_weight, content_current, content_original):
+    """
+    Compute the content loss for style transfer.
+    
+    Inputs:
+    - content_weight: Scalar giving the weighting for the content loss.
+    - content_current: features of the current image; this is a PyTorch Tensor of shape
+      (1, C_l, H_l, W_l).
+    - content_original: features of the content image, Tensor with shape (1, C_l, H_l, W_l).
+
+    Returns:
+    - scalar content loss
+    """
+    ############################################################################
+    # TODO: Compute the content loss for style transfer.                       #
+    ############################################################################
+    _, C, _, _ = content_current.shape
+    c = content_current.clone().squeeze().reshape(C, -1) # current
+    o = content_original.clone().squeeze().reshape(C, -1) # original
+    cl = content_weight * torch.sum((c - o)**2)
+    return cl
+    ############################################################################
+    #                               END OF YOUR CODE                           #
+    ############################################################################
+
+
+def gram_matrix(features, normalize=True):
+    """
+    Compute the Gram matrix from features.
+    
+    Inputs:
+    - features: PyTorch Tensor of shape (N, C, H, W) giving features for
+      a batch of N images.
+    - normalize: optional, whether to normalize the Gram matrix
+        If True, divide the Gram matrix by the number of neurons (H * W * C)
+    
+    Returns:
+    - gram: PyTorch Tensor of shape (N, C, C) giving the
+      (optionally normalized) Gram matrices for the N input images.
+    """
+    gram = None
+    ############################################################################
+    # TODO: Compute the Gram matrix from features.                             #
+    # Don't forget to implement for both normalized and non-normalized version #
+    ############################################################################
+    N, C, H, W = features.shape
+    features = torch.flatten(features, start_dim=2)
+    gram = torch.bmm(features, features.transpose(1,2))
+
+    if normalize:
+       gram /= H*W*C
+    ############################################################################
+    #                               END OF YOUR CODE                           #
+    ############################################################################
+    return gram
+
+
+def style_loss(feats, style_layers, style_targets, style_weights):
+    """
+    Computes the style loss at a set of layers.
+    
+    Inputs:
+    - feats: list of the features at every layer of the current image, as produced by
+      the extract_features function.
+    - style_layers: List of layer indices into feats giving the layers to include in the
+      style loss.
+    - style_targets: List of the same length as style_layers, where style_targets[i] is
+      a PyTorch Tensor giving the Gram matrix of the source style image computed at
+      layer style_layers[i].
+    - style_weights: List of the same length as style_layers, where style_weights[i]
+      is a scalar giving the weight for the style loss at layer style_layers[i].
+      
+    Returns:
+    - style_loss: A PyTorch Tensor holding a scalar giving the style loss.
+    """
+    ############################################################################
+    # TODO: Computes the style loss at a set of layers.                        #
+    # Hint: you can do this with one for loop over the style layers, and       #
+    # should not be very much code (~5 lines).                                 #
+    # You will need to use your gram_matrix function.                          #
+    ############################################################################
+    style_loss = 0
+    for i, l in enumerate(style_layers):
+       target = style_targets[i]
+       weight = style_weights[i]
+       gram = gram_matrix(feats[l])
+
+       style_loss += weight*torch.norm(gram - target, p=2)**2
+      
+    return style_loss
+    ############################################################################
+    #                               END OF YOUR CODE                           #
+    ############################################################################
+
+
+def tv_loss(img, tv_weight):
+    """
+    Compute total variation loss.
+    
+    Inputs:
+    - img: PyTorch Variable of shape (1, 3, H, W) holding an input image.
+    - tv_weight: Scalar giving the weight w_t to use for the TV loss.
+    
+    Returns:
+    - loss: PyTorch Variable holding a scalar giving the total variation loss
+      for img weighted by tv_weight.
+    """
+    ############################################################################
+    # TODO: Compute total variation loss.                                      #
+    # Your implementation should be vectorized and not require any loops!      #
+    ############################################################################
+    _, _, H, W = img.shape
+
+    # Vectorized 
+    h_loss = torch.sum((img[:, :, 1:] - img[:, :, :H-1])**2)
+    w_loss = torch.sum((img[:, :, :, 1:] - img[:, :, :, :W-1])**2)
+    
+    # # Using 2 loops - debuge
+    # # This works, it was used to test whether I understood
+    # # the underlying concept. After this I implemented the 
+    # # vectorised version correctly.
+    # h_loss = 0
+    # w_loss = 0
+    # for i in range(H):
+    #    for j in range(W):
+    #       if i == H - 1 and j == W - 1:
+    #         continue
+    #       elif i == H - 1:
+    #         w_loss += (img[0, :, i, j+1] - img[0, :, i, j])**2
+    #       elif j == W - 1:
+    #         h_loss += (img[0, :, i+1, j] - img[0, :, i, j])**2
+    #       else:
+    #         h_loss += (img[0, :, i+1, j] - img[0, :, i, j])**2
+    #         w_loss += (img[0, :, i, j+1] - img[0, :, i, j])**2
+    # h_loss = torch.sum(h_loss)
+    # w_loss = torch.sum(w_loss)
+          
+
+    return tv_weight*(w_loss + h_loss)
+    ############################################################################
+    #                               END OF YOUR CODE                           #
+    ############################################################################
+
+
+def guided_gram_matrix(features, masks, normalize=True):
+  """
+  Inputs:
+    - features: PyTorch Tensor of shape (N, R, C, H, W) giving features for
+      a batch of N images.
+    - masks: PyTorch Tensor of shape (N, R, H, W)
+    - normalize: optional, whether to normalize the Gram matrix
+        If True, divide the Gram matrix by the number of neurons (H * W * C)
+    
+    Returns:
+    - gram: PyTorch Tensor of shape (N, R, C, C) giving the
+      (optionally normalized) guided Gram matrices for the N input images.
+  """
+  guided_gram = None
+  ##############################################################################
+  # TODO: Compute the guided Gram matrix from features.                        #
+  # Apply the regional guidance mask to its corresponding feature and          #
+  # calculate the Gram Matrix. You are allowed to use one for-loop in          #
+  # this problem.                                                              #
+  ##############################################################################
+  pass
+  ##############################################################################
+  #                               END OF YOUR CODE                             #
+  ##############################################################################
+
+
+def guided_style_loss(feats, style_layers, style_targets, style_weights, content_masks):
+    """
+    Computes the style loss at a set of layers.
+    
+    Inputs:
+    - feats: list of the features at every layer of the current image, as produced by
+      the extract_features function.
+    - style_layers: List of layer indices into feats giving the layers to include in the
+      style loss.
+    - style_targets: List of the same length as style_layers, where style_targets[i] is
+      a PyTorch Tensor giving the guided Gram matrix of the source style image computed at
+      layer style_layers[i].
+    - style_weights: List of the same length as style_layers, where style_weights[i]
+      is a scalar giving the weight for the style loss at layer style_layers[i].
+    - content_masks: List of the same length as feats, giving a binary mask to the
+      features of each layer.
+      
+    Returns:
+    - style_loss: A PyTorch Tensor holding a scalar giving the style loss.
+    """
+    ############################################################################
+    # TODO: Computes the guided style loss at a set of layers.                 #
+    ############################################################################
+    # Replace "pass" statement with your code
+    pass
+    ############################################################################
+    #                               END OF YOUR CODE                           #
+    ############################################################################
